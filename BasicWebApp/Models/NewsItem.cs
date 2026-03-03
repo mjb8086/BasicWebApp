78bf41bb39ea9b8a97263cac819f01eae7afd8bd
mjb8086 <91055630+mjb8086@users.noreply.github.com>
2026-03-03 14:16:06 +0000

    add hax0rz theme, news model

---
+++ BasicWebApp/Models/NewsItem.cs
@@ -0,0 +1,8 @@
+namespace BasicWebApp.Models;
+
+public struct NewsItem
+{
+    public string Title { get; set; }
+    public DateTime When { get; set; }
+    public string Story { get; set; }
+}
--- BasicWebApp/Pages/News.cshtml.cs
+++ BasicWebApp/Pages/News.cshtml.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using BasicWebApp.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BasicWebApp.Pages;
@@ -6,10 +6,19 @@ namespace BasicWebApp.Pages;
 public class NewsModel : PageModel
 {
     private readonly ILogger<NewsModel> _logger;
+    public List<NewsItem> newsItems;
 
     public NewsModel(ILogger<NewsModel> logger)
     {
         _logger = logger;
+        newsItems = new List<NewsItem>()
+        {
+            new()
+            {
+                Title = "Nothing To Report", When = new DateTime(2026, 02, 03, 12, 00, 00),
+                Story = "There is no news, except another war has occurred. Hopefully I don't die."
+            }
+        };
     }
 
     public void OnGet()
