5cd3e12db41689c7f8605c7e4bad16daeed7253e
mjb8086 <91055630+mjb8086@users.noreply.github.com>
2026-03-03 12:50:26 +0000

    add news

---
+++ BasicWebApp/Pages/News.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BasicWebApp.Pages;
+
+public class NewsModel : PageModel
+{
+    private readonly ILogger<NewsModel> _logger;
+
+    public NewsModel(ILogger<NewsModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+
+}
